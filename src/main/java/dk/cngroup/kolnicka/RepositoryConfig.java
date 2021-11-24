3d9f8a1973cc7da189aa2b2f0c49e02293628882
Lukas Spalek <spalek@epages.com>
2021-11-24 22:57:15 +0100

    expose id for Wish entity

---
+++ src/main/java/dk/cngroup/kolnicka/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package dk.cngroup.kolnicka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import dk.cngroup.kolnicka.wishlist.Wish;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+        config.exposeIdsFor(Wish.class);
+    }
+}
